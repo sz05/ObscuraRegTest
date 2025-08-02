c29f7cf81b7bf674a5571174c30c8cc5407a8d2e
sz05 <Shreyans0528@gmail.com>
2025-08-02 16:06:49 +0530

    /play hidden

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/play",
+        destination: "/",
+        permanent: false,
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
